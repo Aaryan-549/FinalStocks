fc8f6f0be73ffc02c8a5ff2e504bd7e6c1b560b2
Aaryan-549 <abeniwal_be23@thapar.edu>
2025-02-03 22:21:01 +0530

    First Commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,45 +1,11 @@
-import React from 'react';
-import { useNavigate} from 'react-router-dom'
-import Navbar from './Components/Navbar';
-import FAQ from './Components/FAQ';
-import LandingPage from './Components/LandingPage';
-import About from './Components/About';
-import Schedule from './Components/schedule';
-import Stocks from './Components/Stocks';
-import StockTracker from './Components/StockTracker';
+import React from 'react'
 
 function Home() {
-  const navigate = useNavigate();
   return (
-    <>
-      {/* Navbar */}
-      <div className="absolute z-10">
-        <Navbar />
-      </div>
-
-      {/* Landing Page */}
-      <LandingPage />
-
-      {/* About Section */}
-      <div id="about" className=''>
-        <About />
-      </div>
-
-      {/* Stocks Section */}
-      <Stocks />
-
-      {/* Schedule Section */}
-      <div id="schedule">
-        <Schedule />
-      </div>
-
-      {/* FAQ Section */}
-      <div id="faq">
-        <FAQ />
-        
-      </div>
-    </>
-  );
+    <div>
+      
+    </div>
+  )
 }
 
-export default Home;
+export default Home
